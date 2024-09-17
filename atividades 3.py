b52c4582a3218fa2e890570ba9d88909ea786f76
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-09-17 08:11:04 -0300

    Delete atividades 3.py

--- atividades 3.py
+++
@@ -1,5 +0,0 @@
-a = float(input("Digite o primeiro número: "))
-b = float(input("Digite o segundo número: "))
-c = float(input("Digite o terceiro número: "))
-media = (a + b + c) / 3
-print("A média é:", media)
